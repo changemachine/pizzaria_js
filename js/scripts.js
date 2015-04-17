6bfa571ea2efcbcf4e62c6e150e46494a474c9c8
james <james@changemachine.org>
2015-04-17 14:42:31 -0700

    Cheese functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
     var PizzaOrder = {
         total: 0,
         cheese: 0,
-        pepp: 0,
-        cheesePlus: function(amount) {
+        pep: 0,
+        chzPlus: function(amount) {
+            if (isNaN(amount)){
+                amount = 0;
+            }
             this.cheese = this.cheese + amount;
+            this.total = this.cheese * 1.00;
         },
-        peppPlus: function(amount) {
-            this.pepp = this.pepp + amount;
+        pepPlus: function(amount) {
+            if (isNaN(amount)){
+                amount = 0;
+            }
+            this.pep = this.pep + amount;
         }
     };
 
@@ -15,17 +22,18 @@ $(document).ready(function() {
     $("#order-form").submit(function(event) {
         event.preventDefault();
 
-        var pizzaType = $('select#pizza-type').val();
-
-        console.log(pizzaType);//--------!
+        var inputChz = $("input#cheese").val();
+        var inputPep = $("input#pep").val();
 
         newPizza = Object.create(PizzaOrder);
-        newPizza.cheese = $('select#pizza-type').val();
+        newPizza.chzPlus(parseInt(inputChz));
+        // newPizza.pep = newPizza.pepPlus(inputPep);
+
+        console.log(newPizza.cheese);
 
 
 
-
-        $("#pizza-price").text(newPizza.total);
+        $("#pizza-price").text("$" + newPizza.total + ".00");
         $("#pizza-price").show();
         $("#order-form").hide();
     });
